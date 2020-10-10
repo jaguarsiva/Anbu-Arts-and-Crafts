80f1c9afd15f240ad2e9a09d1208bcf4096a3d7c
Siva Chandran <60529502+jaguarsiva@users.noreply.github.com>
2020-10-10 15:00:28 +0530

    nav and banner completed

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// Navbar
+const hamburger = document.querySelector(".hamburger");
+const menubar = document.querySelector(".menu-links");
+
+hamburger.addEventListener("click", function() {
+    if( menubar.className === "menu-links" )
+    {
+        hamburger.classList.add("is-active");
+        menubar.className = "menu-links opened menu-open-anm";
+        document.querySelector("html").classList.add("no-scroll");
+    }
+    else
+    {
+        hamburger.classList.remove("is-active");
+        menubar.className = "menu-links opened menu-close-anm";
+        setTimeout(() => {
+            menubar.className = "menu-links";
+        },400);
+        document.querySelector("html").classList.remove("no-scroll");
+    }
+});
+
